88815c045ee3bb2aee7f3891b3c7cd5e50690338
undefinedmamun <bkmmamun@gmail.com>
2018-02-20 12:42:34 +0600

    homepage init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TempComponent } from './temp/temp.component';
 import { PNavComponent } from './p-nav/p-nav.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TempComponent,
-    PNavComponent
+    PNavComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule

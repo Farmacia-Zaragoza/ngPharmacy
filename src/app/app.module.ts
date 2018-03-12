f6583d4659c8772d3141ff9c350f8bbadee0ee82
undefinedMamun <bkmmamun@gmail.com>
2018-03-12 21:47:21 +0600

    category init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule, Http } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { TempComponent } from './temp/temp.component';
 import { PNavComponent } from './p-nav/p-nav.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { CategoryComponent } from './category/category.component';
 import { HomePageService } from './home-page/home-page.service';
 import { PNavService } from './p-nav/p-nav.service';
 
@@ -15,11 +18,16 @@ import { PNavService } from './p-nav/p-nav.service';
     AppComponent,
     TempComponent,
     PNavComponent,
-    HomePageComponent
+    HomePageComponent,
+    CategoryComponent
   ],
   imports: [
-  BrowserModule,
-    HttpModule
+    BrowserModule,
+    HttpModule,
+    [RouterModule.forRoot([
+      { path: '', component: HomePageComponent },
+      { path: 'category/:name', component: CategoryComponent}
+    ])]
   ],
   providers: [
     HomePageService,

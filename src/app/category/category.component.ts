40e9e68dfd3610c334cc8acde251c4a670993961
undefinedMamun <bkmmamun@gmail.com>
2018-03-25 14:00:25 +0600

    category page products fetching from server

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewEncapsulation, AfterViewInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { Component, OnInit, ViewEncapsulation, AfterViewInit, QueryList, ViewChildren } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 declare var $:any;
 @Component({
@@ -10,55 +11,13 @@ declare var $:any;
 export class CategoryComponent implements OnInit, AfterViewInit {
 
   view = "list";
-  products = [
-    {
-      id: 1,
-      name: "A very very Long product name with extra other text",
-      pImage: "assets/images/single_product.jpg",
-      pDes: "Product Description",
-      textField1: "Text field 1",
-      textField2: "Text field 2",
-      price: 978,
-      oldPrice: 999,
-      rating: 50
-    },
-    {
-      id: 2,
-      name: "Product Name",
-      pImage: "assets/images/single_product.jpg",
-      pDes: "Product Description long long",
-      textField1: "Text field 1",
-      textField2: "Text field 2",
-      price: 400,
-      oldPrice: 999,
-      rating: 50
-    },
-    {
-      id: 3,
-      name: "Product Name",
-      pImage: "assets/images/single_product.jpg",
-      pDes: "Product Description",
-      textField1: "Text field 1 with very long text",
-      textField2: "Text field 2",
-      price: 200,
-      oldPrice: 999,
-      rating: 50
-    },
-    {
-      id: 4,
-      name: "Product Name",
-      pImage: "assets/images/single_product.jpg",
-      pDes: "Product Description",
-      textField1: "Text field 1",
-      textField2: "Text field 2 with very long text",
-      price: 300,
-      oldPrice: 999,
-      rating: 50
-    },
-  ]
+  products = [];
+
+  @ViewChildren("singleProduct") singleProduct : QueryList<any>;
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private http: Http
   ) { }
 
   checkAll(input: HTMLInputElement){
@@ -88,6 +47,11 @@ export class CategoryComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit() {
+    this.http.get("https://farma.dbrqx.com/ang/products.json")
+      .subscribe(response => {
+        this.products = response.json();
+      })
+
     this.route.queryParamMap
       .subscribe( params => {
         let viewType = params.get("view");
@@ -97,54 +61,61 @@ export class CategoryComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(){
-    ! function () {
-      var responsiveTexts = $(".responsiveText");
-      responsiveTexts.each(function (index, responsiveText) {
-        var rTextConainerWidth = $(responsiveText).width();
-        if ($(responsiveText).find('p').outerWidth(true) > rTextConainerWidth) {
-          $(responsiveText).addClass('rCollaps').append("<i class='fa fa-plus-circle responsiveTextExpandButton'></i>");
-        }
-      });
-
-
-      $(".responsiveText.rCollaps .responsiveTextExpandButton").click(function () {
-        var parentContainer = $(this).closest('.responsiveText');
-        var rInner = $(parentContainer).find('p').detach();
-        $(parentContainer).toggleClass('rTexpanded');
-        if ($(this).hasClass('fa-plus-circle')) {
-          $(this).removeClass('fa-plus-circle').addClass('fa-minus-circle');
-          $(this).before('<i class="fa fa-angle-right responsiveText_controls"></i>');
-          $(parentContainer).prepend('<div class="rTinner"></div>');
-          $(parentContainer).prepend('<i class="fa fa-angle-left responsiveText_controls"></i>');
-          $(".rTinner").append(rInner);
-        } else {
-          $(this).removeClass('fa-minus-circle').addClass('fa-plus-circle');
-          $(this).siblings('.responsiveText_controls').remove();
-          $(".rTinner").remove();
-          $(parentContainer).prepend(rInner);
-        }
-
-        $(".responsiveText > i.responsiveText_controls").mouseenter(function () {
-          let rTinner = $(this).siblings('.rTinner');
-          let scrollLength = $(rTinner).scrollLeft() + $(rTinner)[0].scrollWidth - $(rTinner).width();
-          let scrollAbleLength = scrollLength - $(rTinner).scrollLeft();
-          if ($(this).hasClass('fa-angle-left')) {
-            $(rTinner).animate({
-              scrollLeft: 0
-            }, 500);
-          }
-          else {
-            $(rTinner).animate({
-              scrollLeft: scrollAbleLength + 10
-            }, 500);
-          }
-        });
-
-        $(".responsiveText > i.responsiveText_controls").mouseleave(function () {
-          $('.rTinner').stop();
-        });
-      });
-    }();
+    this.singleProduct.changes.subscribe( t => {
+      console.log(t);
+      this.responsiveTextInit();
+    })
+    
   }
 
+  responsiveTextInit () {
+    var responsiveTexts = $(".responsiveText");
+    responsiveTexts.each(function (index, responsiveText) {
+      var rTextConainerWidth = $(responsiveText).width();
+      if ($(responsiveText).find('p').outerWidth(true) > rTextConainerWidth) {
+        $(responsiveText).addClass('rCollaps').append("<i class='fa fa-plus-circle responsiveTextExpandButton'></i>");
+      }
+    });
+  
+  
+    $(".responsiveText.rCollaps .responsiveTextExpandButton").click(function () {
+      var parentContainer = $(this).closest('.responsiveText');
+      var rInner = $(parentContainer).find('p').detach();
+      $(parentContainer).toggleClass('rTexpanded');
+      if ($(this).hasClass('fa-plus-circle')) {
+        $(this).removeClass('fa-plus-circle').addClass('fa-minus-circle');
+        $(this).before('<i class="fa fa-angle-right responsiveText_controls"></i>');
+        $(parentContainer).prepend('<div class="rTinner"></div>');
+        $(parentContainer).prepend('<i class="fa fa-angle-left responsiveText_controls"></i>');
+        $(".rTinner").append(rInner);
+      } else {
+        $(this).removeClass('fa-minus-circle').addClass('fa-plus-circle');
+        $(this).siblings('.responsiveText_controls').remove();
+        $(".rTinner").remove();
+        $(parentContainer).prepend(rInner);
+      }
+  
+      $(".responsiveText > i.responsiveText_controls").mouseenter(function () {
+        let rTinner = $(this).siblings('.rTinner');
+        let scrollLength = $(rTinner).scrollLeft() + $(rTinner)[0].scrollWidth - $(rTinner).width();
+        let scrollAbleLength = scrollLength - $(rTinner).scrollLeft();
+        if ($(this).hasClass('fa-angle-left')) {
+          $(rTinner).animate({
+            scrollLeft: 0
+          }, 500);
+        }
+        else {
+          $(rTinner).animate({
+            scrollLeft: scrollAbleLength + 10
+          }, 500);
+        }
+      });
+  
+      $(".responsiveText > i.responsiveText_controls").mouseleave(function () {
+        $('.rTinner').stop();
+      });
+    });
+  }
 }
+
+

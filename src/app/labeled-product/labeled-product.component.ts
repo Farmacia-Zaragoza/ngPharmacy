43dc545c701ecb13243a545b2055742fb056c037
undefinedmamun <bkmmamun@gmail.com>
2018-07-08 22:26:24 +0600

    labeled products inital finish

--- src/app/labeled-product/labeled-product.component.ts
+++ src/app/labeled-product/labeled-product.component.ts
@@ -1,29 +1,49 @@
-import { Component, OnInit, AfterViewInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChildren, QueryList } from '@angular/core';
+import { Http } from '@angular/http';
 declare var $:any;
 
 @Component({
   selector: 'labeled-product',
   templateUrl: './labeled-product.component.html',
-  styleUrls: ['./labeled-product.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./labeled-product.component.css']
 })
 export class LabeledProductComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  products = [];
+  constructor(private http: Http) { }
+  @ViewChildren('labeledSlider') labeledSlider: QueryList <any>;
+
+  slideRight(){
+    $('.prevButton').trigger("click");
+  }
+
+  slideLeft(){
+    $('.nextButton').trigger("click");
+  }
 
   ngOnInit() {
-
+    this.http.get('https://farma.vbrqx.com/ang/labaled_products.json')
+      .subscribe(products=> {
+        this.products = products.json();
+      })
   }
 
   ngAfterViewInit(){
 
+    this.labeledSlider.changes.subscribe(t=>{
+      this.slickInit();
+    })
+
+  }
+
+  slickInit() {
     $('#labeled-product-slider').slick({
       slidesToShow: 4,
       slidesToScroll: 3,
       infinite: true,
-      prevArrow: `<i class="fa fa-angle-left prevButton" aria-hidden="true"></i>`,
-      nextArrow: `<i class="fa fa-angle-right nextButton" aria-hidden="true"></i>`,
-      responsive: [
+      prevArrow: `<span class="prevButton"></span>`,
+      nextArrow: `<span class="nextButton"></span>`,
+      responsspanve: [
         {
           breakpoint: 767,
           settings: {
@@ -40,7 +60,6 @@ export class LabeledProductComponent implements OnInit, AfterViewInit {
         }
       ]
     });
-
   }
 
 }
